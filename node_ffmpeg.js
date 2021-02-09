3952e484e4da7c7426c9d3ce2d803e0d98357167
highK <restartplant@gmail.com>
2021-02-09 02:12:52 -0800

    better

--- node_ffmpeg.js
+++ node_ffmpeg.js
@@ -5,26 +5,38 @@ var express = require("express");
 var app = express();
 const DB = require('./DB');
 
-var port = argv.port || 8080;
+var port = 8080;
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 
-app.post('exec', (req, res, next) => {
+app.get('/', (req, res, next) => {
+	console.log('get');
+	res.send('get');
 
-	DB.query(`SELECT * FROM xe_stream_manager WHERE stream_key = ?`, [req.body.name], (err, results) => {
+});
+
+
+app.post('/exec/:id', (req, res, next) => {
+	const id = req.params.id;
+	if(!id) {
+		res.status(400);
+		next();
+	}
+
+	DB.query(`SELECT * FROM xe_stream_manager WHERE stream_key = ?`, [id], (err, results) => {
 		console.log(results);
 
 		if(true) return;
 
 		results.forEach(stream => {
 			if(stream.type === 0)
-				exec(`ffmpeg -y -i rtmp://0.0.0.0/lives/${req.body.name} -acodec libopus -vn -f rtp rtp://0.0.0.0:${stream.id}`);
+				exec(`ffmpeg -y -i rtmp://0.0.0.0/lives/${id} -acodec libopus -vn -f rtp rtp://0.0.0.0:${stream.id}`);
 			else if(stream.type === 1)
-				exec(`ffmpeg -y -i rtmp://0.0.0.0/lives/${req.body.name} -c:v copy -preset:v ultrafast -tune zerolatency -g 60 -an -f rtp rtp://0.0.0.0:${stream.id}?pkt_size=1300`);
+				exec(`ffmpeg -y -i rtmp://0.0.0.0/lives/${id} -c:v copy -preset:v ultrafast -tune zerolatency -g 60 -an -f rtp rtp://0.0.0.0:${stream.id}?pkt_size=1300`);
 			else if(stream.type === 2)
-				exec(`ffmpeg -y -i rtmp://0.0.0.0/lives/${req.body.name} -c:v libx264 -preset:v ultrafast -s 480x270 -tune zerolatency -an -g 60 -f rtp rtp://0.0.0.0:${stream.id}?pkt_size=1300`);
+				exec(`ffmpeg -y -i rtmp://0.0.0.0/lives/${id} -c:v libx264 -preset:v ultrafast -s 480x270 -tune zerolatency -an -g 60 -f rtp rtp://0.0.0.0:${stream.id}?pkt_size=1300`);
 		});
 	})
 	res.status(200);
